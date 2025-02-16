1f560c24e6f80b9e2882e7b11a40b042a90f62ed
yash-shewalkar <iamyashshewalkar@gmail.com>
2025-02-16 18:46:44 +0530

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['fonts.googleapis.com'],
+  },
+
 };
 
 export default nextConfig;
